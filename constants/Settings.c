45ebec18407e30130c0b6e1d84366584f2622495
Ian Shi <Ianshi1026@gmail.com>
2024-05-29 15:23:57 -0400

    remove unused stuff

--- constants/Settings.c
+++ constants/Settings.c
@@ -4,7 +4,6 @@ int drivetrainWidth = 20; // find the real value for this, in inches
 int driveDeadband = 5;
 int feetToEncoderUnits = 344;
 int inchesToEncoderUnits = 28.7
-int lineFollowerThreshhold = 300; // line follower detects values under this (brighter light means lower value)
 
 // arm
 float armControllerSensitivity = 0.5; // higher values means setpoint will change faster
