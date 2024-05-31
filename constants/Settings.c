ea16f76d5d575b6c5be032d2111c339b07c92ea6
Ian Shi <Ianshi1026@gmail.com>
2024-05-31 08:31:26 -0400

    change some constants

--- constants/Settings.c
+++ constants/Settings.c
@@ -3,13 +3,11 @@ bool squareInputs = true;
 int drivetrainWidth = 20; // find the real value for this, in inches
 int driveDeadband = 5;
 int feetToEncoderUnits = 344;
-int inchesToEncoderUnits = 28.7
+int inchesToEncoderUnits = 28.7;
 
 // arm
-int maxArmAngle = 1175;
+int maxArmAngle = 850;
 int minArmAngle = 10;
-int armHoldStrength = 15;
+int armHoldStrength = 18;
 int maxArmError = 40;
 float armkP = .45;
-
-// claw
