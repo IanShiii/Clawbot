a7f9a9968e73320bc87de994c8d6e8926b67b920
Ian Shi <Ianshi1026@gmail.com>
2024-06-01 13:27:30 -0400

    autons

--- autons/Autons.c
+++ autons/Autons.c
@@ -1,11 +1,27 @@
-task ScoreDriveBack() {
+task scoreDriveBack() {
 	startTask(armFollowSetpoint);
 	setArmSetpoint(maxArmAngle);
-	driveForwardDistance(4.95);
+	driveForwardDistance(5);
 	wait1Msec(1000);
 	setArmSetpoint(200);
+	wait1Msec(1750);
+    driveBackwardsDistance(1.5);
+    while(true) {
+        abortTimeslice();
+    }
+}
+
+// turns right
+task scoreThenGoToNextPieceRed() {
+	startTask(armFollowSetpoint);
+	setArmSetpoint(maxArmAngle);
+	driveForwardDistance(6);
 	wait1Msec(1000);
-    driveBackwardsDistance(3);
+	setArmSetpoint(200);
+	wait1Msec(1500);
+    driveBackwardsDistance(1);
+	turnDegreesRight(90);
+	driveForwardDistance(3);
     while(true) {
         abortTimeslice();
     }
